f6e273da50145d36b91c56fa0ba7b074d80b105e
craigmc08 <craigmc08@gmail.com>
2019-03-03 18:05:54 -0500

    Create FilterLifts function

--- src/lift-filter.js
+++ src/lift-filter.js
@@ -10,6 +10,17 @@ for accelerations that are less than a certain threshold, mark these points as l
 for points that are surrounded on both sides within some threshold of time by lift points,
 mark these as lifts
 
+remove all lift points
+
 done
 
 */
+
+/**
+ * Remove detected lift rides from a track
+ * @param {Object[]} track - Array of all gps points
+ * @returns {Object[]} Array of gps points that are not lift rides
+ */
+module.exports = function FilterLifts(track) {
+    return track;
+}
