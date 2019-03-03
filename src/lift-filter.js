31ccde952eb3f3126e670b804e6c6ca4419f42e8
craigmc08 <craigmc08@gmail.com>
2019-03-03 18:11:44 -0500

    Add function to calculate velocities of points

--- src/lift-filter.js
+++ src/lift-filter.js
@@ -7,14 +7,45 @@ make array of this
 find acceleration of each gps point
 
 for accelerations that are less than a certain threshold, mark these points as lifts
+
 for points that are surrounded on both sides within some threshold of time by lift points,
-mark these as lifts
+mark these as lifts. this ensures slight turns are changes of direction of the lift is counted
 
 remove all lift points
 
 done
 
 */
+
+const arcLength = radius => angle => radius * angle;
+const earthRadius = 20900000; // In feet
+const earthArcLength = arcLength(earthRadius);
+
+const calculateVelocities = track => {
+    const velocities = [];
+    for (let i = 0; i < track.length; i++) {
+        const startPoint = track[i === 0 ? 0 : i - 1];
+        const endPoint = track[i === 0 ? 1 : i];
+
+        const deltaLatitude = endPoint.latitude - startPoint.latitude;
+        const deltaLongitude = endPoint.longitude - startPoint.longitude;
+        
+        const deltaLatFeet = earthArcLength(deltaLatitude);
+        const deltaLonFeet = earthArcLength(deltaLongitude);
+        const deltaEleFeet = endPoint.elevation - startPoint.elevation;
+
+        const magnitude = Math.sqrt(deltaLatFeet*deltaLatFeet + deltaLonFeet*deltaLonFeet + deltaEleFeet*deltaEleFeet);
+        const velocity = [
+            deltaLatFeet / magnitude,
+            deltaLonFeet / magnitude,
+            deltaEleFeet / magnitude,
+        ];
+
+        velocities.push(velocity);
+    }
+    
+    return velocities;
+}
 
 /**
  * Remove detected lift rides from a track
