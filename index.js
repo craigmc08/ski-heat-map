173f5bc8f11436fa875c9f106f1a68183f88a6a1
craigmc08 <craigmc08@gmail.com>
2019-03-03 19:57:47 -0500

    Adjust default settings to include lift-filter

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const liftFilter = require('./src/lift-filter');
+
 require('./src/main')({
     tracksDir: './tracks',
     outputFile: './heatmap.png',
     imageWidth: 512,
-    coordinatePaddingPercent: 0.05,
+    coordinatePaddingPercent: 0.01,
+    filters: [
+        liftFilter({}),
+    ],
 }).catch(err => {
     console.error(err);
 });
